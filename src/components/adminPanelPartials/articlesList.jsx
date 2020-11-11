2f8a29032b12a316e13d5b95a4c42a24fa140be1
Artem Tarasenko <artem.t.88@ya.ru>
2020-11-12 00:26:02 +1000

    Add ArticleLisy layote

---
+++ src/components/adminPanelPartials/articlesList.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import AdminHeader from "./adminHeader";
+import Footer from "./footer";
+import Fab from "@material-ui/core/Fab";
+import DeleteOutlineIcon from '@material-ui/icons/DeleteOutline';
+
+function ArticlesList() {
+
+    function submitNote(){
+
+    }
+
+	return (
+        <main role="main" className="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
+        <AdminHeader />
+        <div className="container-fluid">
+            <div className="d-flex flex-column align-items-start col-md-10">
+                <h3>Список всех статей</h3>
+                <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+                <hr />
+                <nav className="navbar navbar-light bg-secondary col-md-12">
+                    <form className="form-inline">
+                        <input className="form-control mr-sm-2" type="search" placeholder="Поиск" aria-label="Search" />
+                    </form>
+                </nav>
+                <div className="list-group-flush col-md-12">
+                    <li href="#" className="list-group-item d-flex justify-content-between">
+                        <a href="#" className="align-self-center">Caras justo odio</a>
+                        <span className="article-list-fab">
+                            <button type="button" className="btn btn-sm btn-outline-secondary mr-2">Удалить</button>
+                            {/* <a href="#" className="badge badge-danger">X</a> */}
+                            <Fab color="inherit" onClick={submitNote}>
+                                <DeleteOutlineIcon />
+                            </Fab>
+                        </span>
+                    </li>
+                    <li href="#" className="list-group-item  d-flex justify-content-between">
+                        <a href="#" className="align-self-center">Caras justo odio</a>
+                        <span className="article-list-fab">
+                            <button type="button" className="btn btn-sm btn-outline-secondary mr-2">Удалить</button>
+                            {/* <a href="#" className="badge badge-danger">X</a> */}
+                            <Fab color="inherit" onClick={submitNote}>
+                                <DeleteOutlineIcon />
+                            </Fab>
+                        </span>
+                    </li>
+                </div>
+            </div>
+        </div>
+        </main>
+	)
+}
+
+export default ArticlesList;
--- src/components/adminPanelPartials/menu.jsx
+++ src/components/adminPanelPartials/menu.jsx
@@ -1,101 +1,107 @@
 import React from "react";
-
+import SearchIcon from '@material-ui/icons/Search';
+import ClearIcon from '@material-ui/icons/Clear';
 
 function Menu() {
 	return (
-		<nav class="col-md-2 d-none d-md-block bg-light sidebar">
-		  <div class="sidebar-sticky">
-		  	<h2>МЕНЮ</h2>
-		  	<form class="form-inline">
-			  <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-			  <button class="btn btn-outline-success my-2 my-sm-0" type="submit"></button>
+		<nav className="col-md-2 d-none d-md-block bg-light sidebar">
+		  <div className="sidebar-sticky">
+		  	<div className="col">
+			  <h2>МЕНЮ</h2>
+		  	<form className="form-inline search d-flex">
+			  <input className="form-control col-md-8" type="search" placeholder="Search" aria-label="Search" />
+			  <div class="btn-group btn-group-toggle col-md-3" data-toggle="buttons">
+			  	<button className="btn btn-outline-secondary my-2 my-sm-0" type="submit"><SearchIcon /></button>
+				<button className="btn btn-outline-secondary my-2 my-sm-0" type="submit"><ClearIcon /></button>
+			  </div>
 			</form>
-			<h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+			<h6 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
 			  <span>СПИСОК РАЗДЕЛОВ</span>
 			</h6>
-			<ul class="nav flex-column mb-5 menu-level-1 align-items-start">
-			  <li class="nav-item">
-				<a class="nav-link active" href="#">
+			<ul className="nav flex-column mb-5 menu-level-1 align-items-start">
+			  <li className="nav-item">
+				<a className="nav-link active" href="#">
 				  <span data-feather="home"></span>
-				  Пункт 1 <span class="sr-only">(current)</span>
+				  Пункт 1 <span className="sr-only">(current)</span>
 				</a>
 			  </li>
-			  <ul class="nav flex-column menu-level-3 ml-4 align-items-start">
-			  	<li class="nav-item">
-			  	  <a class="nav-link active" href="#">
+			  <ul className="nav flex-column menu-level-3 ml-4 align-items-start">
+			  	<li className="nav-item">
+			  	  <a className="nav-link active" href="#">
 			  		<span data-feather="home"></span>
-			  		Пункт 1 <span class="sr-only">(current)</span>
+			  		Пункт 1 <span className="sr-only">(current)</span>
 			  	  </a>
 			  	</li>
-			  	<li class="nav-item">
-			  	  <a class="nav-link" href="#">
+			  	<li className="nav-item">
+			  	  <a className="nav-link" href="#">
 			  		<span data-feather="file"></span>
 			  		Пункт 2
 			  	  </a>
 			  	</li>
 			  </ul>
-			  <li class="nav-item">
-				<a class="nav-link" href="#">
+			  <li className="nav-item">
+				<a className="nav-link" href="#">
 				  <span data-feather="shopping-cart"></span>
 				  Пункт 3
 				</a>
 			  </li>
-			  <li class="nav-item">
-				<a class="nav-link" href="#">
+			  <li className="nav-item">
+				<a className="nav-link" href="#">
 				  <span data-feather="users"></span>
 				  Пункт 4
 				</a>
 			  </li>
-			  <ul class="nav flex-column mb-5 menu-level-2 ml-4 align-items-start">
-	  			  <li class="nav-item">
-	  				<a class="nav-link active" href="#">
+			  <ul className="nav flex-column mb-5 menu-level-2 ml-4 align-items-start">
+	  			  <li className="nav-item">
+	  				<a className="nav-link active" href="#">
 	  				  <span data-feather="home"></span>
-	  				  Пункт 1 <span class="sr-only">(current)</span>
+	  				  Пункт 1 <span className="sr-only">(current)</span>
 	  				</a>
 	  			  </li>
-	  			  <li class="nav-item">
-	  				<a class="nav-link" href="#">
+	  			  <li className="nav-item">
+	  				<a className="nav-link" href="#">
 	  				  <span data-feather="file"></span>
 	  				  Пункт 2
 	  				</a>
 	  			  </li>
-				  <ul class="nav flex-column menu-level-3 ml-4 align-items-start">
-		  			  <li class="nav-item">
-		  				<a class="nav-link active" href="#">
+				  <ul className="nav flex-column menu-level-3 ml-4 align-items-start">
+		  			  <li className="nav-item">
+		  				<a className="nav-link active" href="#">
 		  				  <span data-feather="home"></span>
-		  				  Пункт 1 <span class="sr-only">(current)</span>
+		  				  Пункт 1 <span className="sr-only">(current)</span>
 		  				</a>
 		  			  </li>
-		  			  <li class="nav-item">
-		  				<a class="nav-link" href="#">
+		  			  <li className="nav-item">
+		  				<a className="nav-link" href="#">
 		  				  <span data-feather="file"></span>
 		  				  Пункт 2
 		  				</a>
 		  			  </li>
-		  			  <li class="nav-item">
-		  				<a class="nav-link" href="#">
+		  			  <li className="nav-item">
+		  				<a className="nav-link" href="#">
 		  				  <span data-feather="shopping-cart"></span>
 		  				  Пункт 3
 		  				</a>
 		  			  </li>
-		  			  <li class="nav-item">
-		  				<a class="nav-link" href="#">
+		  			  <li className="nav-item">
+		  				<a className="nav-link" href="#">
 		  				  <span data-feather="users"></span>
 		  				  Пункт 4
 		  				</a>
 		  			  </li>
 	  				</ul>
-	  			  <li class="nav-item">
-	  				<a class="nav-link" href="#">
+	  			  <li className="nav-item">
+	  				<a className="nav-link" href="#">
 	  				  <span data-feather="users"></span>
 	  				  Пункт 4
 	  				</a>
 	  			  </li>
   			</ul>
 			</ul>
-			<div class="d-flex flex-column align-items-center">
-				<button class="btn btn-secondary col-md-9 mb-2 mt-2" type="submit">Список статей</button>
-				<button class="btn btn-secondary col-md-9 mb-2 mt-2" type="submit">Редактировать разделы</button>
+			<div className="d-flex flex-column align-items-center">
+				<button className="btn btn-secondary col-md-9 mb-2 mt-2" type="submit">Список статей</button>
+				<button className="btn btn-secondary col-md-9 mb-2 mt-2" type="submit">Редактировать разделы</button>
+			</div>
 			</div>
 		  </div>
 		</nav>
