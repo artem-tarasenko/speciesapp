28ad33a8524706c5c93a9c046a0700df3c9c1c1f
Артем Г. Тарасенко <tarasenko_ag@ocean.local>
2020-12-07 16:48:18 +1000

    breadcrumbs

--- src/components/appPartials/TestRouting.jsx
+++ src/components/appPartials/TestRouting.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import {Cats, SubcatsDB, ArticlesDB} from "./testdata";
+import {CategoriesDB, ArticlesDB} from "./testdata";
 
 import {
   Switch,
@@ -9,99 +9,87 @@ import {
 } from "react-router-dom";
 
 
-const categories = [
-  { id: "0", title: "Kingdom: Animal", subcategories: [1, 2, 3] },
-  { id: 1, title: "Phylum: Chordata with SUBCATS", subcategories: [4, 5, 6, 8, 9, 10] },
-  { id: 2, title: "Clade: Synapsida with ARTICLES", articles: [1, 2] },
-  { id: 3, title: "Class: Mammailia with DESCRIPTION", description: 10},
-  { id: 4, title: "Order: Carnivora", articles: [1, 2]  },
-  { id: 5, title: "Subfamily: Felinae", articles: [1, 2, 3, 4] },
-  { id: 6, title: "Genus: Felis", articles: [5, 6, 7] },
-  { id: 7, title: "Species: Felis catus", articles: [8] },
-  { id: 8, title: "Order: Carnivora", articles: [1, 5, 7, 9] },
-  { id: 9, title: "Subfamily: Felinae", articles: [2, 4, 6, 8] },
-  { id: 10, title: "Genus: Felis", articles: [1, 5, 7, 10] },
-];
+const Breadcrumbs = () => <Route path="*" render={props => {
+    let parts = props.location.pathname.split("/"); //get all ID's from the path
+    const place = parts[parts.length-1]; // save last one
 
-const articles = [
-  { id: 1, title: "Article 1 Chordata"},
-  { id: 2, title: "Article 2 Synapsida"},
-  { id: 3, title: "Article 3 Mammailia"},
-  { id: 4, title: "Article 4 Carnivora"},
-  { id: 5, title: "Article 5 Felinae"},
-  { id: 6, title: "Article 6 Felis"},
-  { id: 7, title: "Article 7 Felis catus"},
-  { id: 8, title: "Article 8 Carnivora"},
-  { id: 9, title: "Article 9 Felinae"},
-  { id: 10, title: "DESCR for subcar 3 Felis"},
-];
+//==============================================================
 
 
+	parts = parts.slice(1, parts.length-1); //remove the last one from the array
 
-// function RenderMenu2(props) {
-//     //destructuring passed match object to extract params from it as variables
-//     const {category, subcategory, article} = props.match.params;
+	console.log("========= Breadcrums ========");
+	// console.log("Place var ->");
+	// console.log(place);
+	// console.log("Parts array ->");
+	// console.log(parts);
 
-//     //build strings from variables to use as routes
-//     const link1 = category; //ID
-//     const link2 = category + "/" + subcategory;
-//     const link3 = category + "/" + subcategory + "/" + article;
-//     //find titles for links
-//     const link1Name = findTitle(category, "category");
-//     const link2Name = findTitle(subcategory, "subcategory");
-//     const link3Name = findTitle(article, "article");
+	let pathObject = [{link: "/", name: "RooT"}];
+	let lastCrumsItem;
 
-//     //function to find title in data arrays using params: ID and type of item
-//     function findTitle(targetItem, type) { //where item = ID
-//         let linkTitle;
-//         if (type === "category" || type === "subcategory") {
-//             linkTitle = categories.find( item => item.id == targetItem).title;
-//         } else if (type === "article") {
-//             linkTitle = articles.find( item => item.id == targetItem).title;
-//         } 
-//         return linkTitle;
-//     }
+	//somewhere here I need to make an array of objects that containt proper names for ID's whether ID is cat or articles
+	if (parts.length > 0 || place > 0) {
+		console.log("parts exist");
+		if (parts.length < 1) {
+			//starting with PLACE variable
+			lastCrumsItem = CategoriesDB.find( item => item.id == place);
+		} else {
+			//must declare lastCrumsItem after first ID
+		}
 
 
 
-//     //menu component with links to all levels
-//     return (
-//             <nav className="sidebar container d-flex flex-column">
-//                 <Link to="/">HOME</Link>
-//                 { category && <Link to={`/${link1}`}>{link1Name}</Link> }
-//                 { subcategory && <Link to={`/${link2}`}>{link2Name}</Link> }
-//                 { article && <NavLink to={`/${link3}`} activeClassName="active">{link3Name}</NavLink> }
-//             </nav>
-//     )
 
-// }
 
-// function RenderMenu(props) {
-//     const {path, setPath} = useState([{url: "/", name: "ROOT"}])
 
-//     console.log("==== MENU STATEU PDATED");
-//     console.log(path);
-// }
 
-// function updateMenu({path}, action) {
-//     setPath( (prevValue) => [...prevValue, path]);
-// }
 
-function testfunc(evt) {
-    console.log(evt.target);
-}
 
+	} else {
+		console.log("parts EMPTY");
+	}
+
+
+
+//==============================================================
+
+
+
+    return (
+			<div className="nav-menu d-flex flex-column">
+				<div className="container">
+					<p><Link to="/" >ROOT</Link></p>
+					{parts.map(crumb)}
+					<p className="active link">{lastCrumsItem && lastCrumsItem.title}</p>
+				</div>
+			</div>
+		)
+	}}
+/>
+
+const crumb = (part, partIndex, parts) => {
+        const path = ['', ...parts.slice(0, partIndex+1)].join("/");
+        return <p><Link key={path} to={path} >{part}</Link></p>}
+
+
+//============================================================================
+//===========================   RENDER CONTENT  ==============================
+// This function gets props from calling, normally parent ID and router match
+// With this data it return 3 different types of content based on what properties
+//parent category has: articles, subcategories or description
+//Used to render 2 and 3 levels of categories tree
+//============================================================================
 function ConditionalContentRender(props) {
     let parent = props.parent;
     let match = props.match;
 
     if (parent.hasOwnProperty("subcategories")) {
-        console.log(match);
         console.log("ConditionalContentRender(): rendering subcategories...");
         return (
-            <div className="container">
+            <>
                 {match.isExact && (
                     <>
+						<section className="container">
                         <h3>{parent.title}</h3>
                         <hr />
                         <p>parent found, id is {parent.id}</p>
@@ -109,24 +97,26 @@ function ConditionalContentRender(props) {
                             return (
                             <div>
                                 <Link key={subCategoryId} to={`${match.url}/${subCategoryId}`}>
-                                {categories.find(cat => cat.id === subCategoryId).title} ***
+                                {CategoriesDB.find(cat => cat.id === subCategoryId).title} ***
                                 </Link>
                             </div>
                             );
                         })}
+						</section>
                     </>
                 )}
                 <Switch>
                     <Route path={`${match.path}/:subcategory`} component={Subcategory} />
                 </Switch>
-            </div>
+            </>
         )
     } else if (parent.hasOwnProperty("articles")) {
-        console.log("ConditionalContentRender(): rendering articles...");
+        // console.log("ConditionalContentRender(): rendering articles...");
         return (
-            <div className="container">
-                {match.isExact && (
-                    <>
+            <>
+				{match.isExact && (
+					<>
+						<section className="container">
                         <h3>{parent.title}</h3>
                         <hr />
                         <p>parent found, id is {parent.id}</p>
@@ -134,34 +124,47 @@ function ConditionalContentRender(props) {
                             return (
                             <div>
                                 <Link key={articleId} to={`${match.url}/${articleId}`}>
-                                    {articles.find(art => art.id === articleId).title} ---- *#*
+                                    {ArticlesDB.find(art => art.id === articleId).title} ---- *#*
                                 </Link>
                             </div>
                             );
                         })}
-
-                    </>
-                )}
+						</section>
+					</>
+				)}
                 <Switch>
                     <Route path={`${match.path}/:article`} component={Article} />
                 </Switch>
-            </div>
+
+            </>
         )
     } else if (parent.hasOwnProperty("description")) {
-        console.log("ConditionalContentRender(): rendering description...");
-        let descArticle =  articles.find( art => art.id === parent.description)
-        return (
-            <div className="container">
-                {match.isExact && (
-                    <>
-                        <h3>{descArticle.title}</h3>
-                        <hr />
-                        <p>Some text here from {descArticle.title}</p>
-                        <p>parent found, id is {parent.id}</p>
-                    </>
-                )}
-            </div>
-        )
+        // console.log("ConditionalContentRender(): rendering description...");
+		let descArticle;
+		if (parent.description <= ArticlesDB.length) {
+			descArticle = ArticlesDB.find( item => item.id == parent.description);
+	        return (
+				<>
+					<section className="container">
+	                    <h3>{descArticle.title}</h3>
+	                    <hr />
+	                    <p>Some text here from {descArticle.title}</p>
+	                    <p>{descArticle.subtitle}</p>
+						<p>{descArticle.content}</p>
+					</section>
+				</>
+			)
+		} else {
+			console.log("Parent category does not have any description, or article does not found");
+			return (
+				<>
+					<h3>Something went wrong!</h3>
+					<hr />
+					<p>Parent category does not have any description, or article does not found</p>
+				</>
+			)
+		}
+
     } else {
         console.log("Something wrong with testing parent category");
     }
@@ -173,19 +176,20 @@ function ConditionalContentRender(props) {
 //============================  SINGLE ARTICLE  ==============================
 function Article({match}) {
     //getting what needs to be rendered
-    const article = articles.find( art => parseInt(match.params.article) === art.id );
+    const article = ArticlesDB.find( art => parseInt(match.params.article) === art.id );
 
     return (
         <>
-            <div className="container">
-                {match.isExact && (
-                    <>
-                        <h3>{article.title}</h3>
-                        <hr />
-                        <p>Article found, id is {article.id}</p>
-                    </>
-                )}
-            </div>
+            {match.isExact && (
+                <>
+					<section className="container">
+	                    <h2>{article.title}</h2>
+						<h5>{article.subtitle}</h5>
+						<hr />
+	                    <p>{article.content}</p>
+					</section>
+                </>
+            )}
         </>
     )
 }
@@ -195,61 +199,53 @@ function Article({match}) {
 //===============================  ARTICLES  =================================
 function Subcategory({match}) {
     //getting what needs to be rendered
-    const subcategory = categories.find( category => parseInt(match.params.subcategory) === category.id );
+    const subcategory = CategoriesDB.find( category => parseInt(match.params.subcategory) === category.id );
 
     //---------------------------------------------------------------
 
     return (
         <>
-        <ConditionalContentRender parent={subcategory} match={match} logging={"rendered Subcat"} />        
-
+        	<ConditionalContentRender parent={subcategory} match={match} />
         </>
     )
 }
-
 
 //============================================================================
 //=========================   RENDER LEVEL 2 PAGE  ===========================
 //=============================  SUBCATEGORIES  ==============================
 function Category({match}) {
     //getting what needs to be rendered
-    const category = categories.find( category => parseInt(match.params.category) === category.id );
+    const category = CategoriesDB.find( category => parseInt(match.params.category) === category.id );
 
     return (
-        <>  
-            <ConditionalContentRender parent={category} match={match} logging={"rendered Category"} />
-
+        <>
+            <ConditionalContentRender parent={category} match={match} />
         </>
     )
 }
-
 
 //============================================================================
 //=========================   RENDER LEVEL 1 PAGE  ===========================
 //==============================  CATEGORIES  ================================
 function Home({match}) {
-    const rootCategory = categories.find(item => item.id == 0);
-    let rootCategories = rootCategory.subcategories.map(subcatID => categories.find(item => item.id == subcatID));
-    
+    const rootCategory = CategoriesDB.find(item => item.id == 0);
+    let rootCategories = rootCategory.subcategories.map(subcatID => CategoriesDB.find(item => item.id == subcatID));
+
     return (
         <>
-            <div className="container">
-                {match.isExact && (
-                    <>
-                        <h3>{rootCategory.title}</h3>
-                        <hr />
-                        {rootCategories.map(subCat => {
-                            return (
-                            <div key={subCat.id}>
-                                <Link to={`${subCat.id}`} onClick="testfunc">
-                                    {categories.find(cat => cat.id === subCat.id).title} ***
-                                </Link>
-                            </div>
-                            );
-                        })}
-                    </>
-                )}
-            </div>
+            <section className="container">
+                <h3>{rootCategory.title}</h3>
+                <hr />
+                {rootCategories.map(subCat => {
+                    return (
+                    <div key={subCat.id}>
+                        <Link to={`${subCat.id}`}>
+                            {CategoriesDB.find(cat => cat.id === subCat.id).title} ***
+                        </Link>
+                    </div>
+                    );
+                })}
+            </section>
         </>
 
     )
@@ -261,12 +257,13 @@ function Home({match}) {
 //============================================================================
 function TestRouting() {
     return (
-        <>
-        <Link to="/">HOME</Link>
-        <Switch>
+        <><div className="main d-flex flex-row">
+		<Breadcrumbs />
+	    <Switch>
             <Route exact path={`/`} component={Home} />
-            <Route path={`/:category`} component={Category} />
+			<Route path={`/:category`} component={Category} />
         </Switch>
+		</div>
         </>
     )
 }
--- src/components/appPartials/testdata.jsx
+++ src/components/appPartials/testdata.jsx
@@ -1,58 +1,59 @@
-const Cats = [
-    {id: "1", title: "title 1 with items", hasArticle: false, hasSubcategories: false, hasItems: true},
-    {id: "2", title: "title 2 with descr", hasArticle: true, hasSubcategories: false, hasItems: false},
-    {id: "3", title: "title 3 with items", hasArticle: false, hasSubcategories: false, hasItems: true},
-	{id: "4", title: "title 4 with subcats", hasArticle: false, hasSubcategories: true, hasItems: false},
-	{id: "7", title: "title 7 with items", hasArticle: false, hasSubcategories: false, hasItems: true},
-	{id: "8", title: "title 8 with subcats", hasArticle: false, hasSubcategories: true, hasItems: false},
-	{id: "9", title: "title 9 with items", hasArticle: false, hasSubcategories: false, hasItems: true},
-	{id: "10", title: "title 10 with items", hasArticle: false, hasSubcategories: false, hasItems: true},
-];
-
-const SubcatsDB = [
-    {id: "5", title: "sub title 5", parent: "4"},
-    {id: "6", title: "sub title 6", parent: "4"},
-    {id: "11", title: "sub title 11", parent: "8"},
-    {id: "12", title: "sub title 12", parent: "8"},
-    {id: "13", title: "sub title 13", parent: "8"}
+const CategoriesDB = [
+  { id: "0", title: "Дальневосточные моря", subcategories: [1, 2, 3] },
+  { id: 1, title: "L1 Phylum: Chordata with SUBCATS", subcategories: [4, 5, 6, 8, 9, 10] },
+  { id: 2, title: "L1 Clade: Synapsida with ARTICLES", articles: [1, 2] },
+  { id: 3, title: "L1 Class: Mammailia with DESCRIPTION", description: [5]},
+  { id: 4, title: "L2 Order: Carnivora", articles: [1, 2]  },
+  { id: 5, title: "L2 Subfamily: Felinae", articles: [1, 2, 3, 4] },
+  { id: 6, title: "L2 Genus: Felis", articles: [5, 6, 7] },
+  { id: 7, title: "L2 Species: Felis catus", articles: [6] },
+  { id: 8, title: "L2 Order: Carnivora", articles: [1, 5, 7] },
+  { id: 9, title: "L2 Subfamily: Felinae", articles: [2, 4, 6] },
+  { id: 10, title: "L2 Genus: Felis", articles: [1, 5, 7] },
 ];
 
 const ArticlesDB = [
-    {id: "14", parent: "1", ancestor: ["1", "s1"],
+    {
+		id: 1,
+		parent: 1,
+		ancestor: ["1", "s1"],
         title: "Article 14 from cat #1",
         subtitle: "This is a testing subtitle.",
-        content: "fasd asdf sdf asdf asdf sdf asdf asd"
+        content: "This is the article with a description of category. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
     },
-    {id: "15", parent: "2", ancestor: ["2", "s2"],
+    {
+		id: 2,
+		parent: 2,
+		ancestor: ["2", "s2"],
         title: "Article 15 from cat #2",
         subtitle: "This is a testing subtitle.",
         content: "This is the article with a description of category. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
     },
-    {id: "11", parent: "1", ancestor: ["1", "s1"],
+    {id: 3, parent: 1, ancestor: ["1", "s1"],
         title: "Article 16 from cat #1",
         subtitle: "This is a testing subtitle.",
         content: "This is the article with a description of category. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
     },
-    {id: "17", parent: "3", ancestor: ["3", "s3"],
+    {id: 4, parent: 3, ancestor: ["3", "s3"],
         title: "Article 17 from cat #3",
         subtitle: "This is a testing subtitle.",
         content: "This is the article with a description of category. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
     },
-    {id: "18", parent: "5", ancestor: ["4", "5"],
+    {id: 5, parent: 5, ancestor: ["4", "5"],
         title: "Article 18 from subcat #5",
         subtitle: "This is a testing subtitle.",
         content: "This is the article with a description of category. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
     },
-    {id: "19", parent: "5", ancestor: ["4", "5"],
+    {id: 6, parent: 5, ancestor: ["4", "5"],
         title: "Article 19 from subcat #5",
         subtitle: "This is a testing subtitle.",
         content: "This is the article with a description of category. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
     },
-    {id: "20", parent: "5", ancestor: ["4", "5"],
+    {id: 7, parent: 5, ancestor: ["4", "5"],
         title: "Article 20 from subcat #5",
         subtitle: "This is a testing subtitle.",
         content: "This is the article with a description of category. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
     }
 ];
 
-export {Cats, SubcatsDB, ArticlesDB};
+export {CategoriesDB, ArticlesDB};
